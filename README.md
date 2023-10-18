10af181b392790749a31c8bc6fe2e529544dffcb
Diogo Trindade <diogotr7@gmail.com>
2023-10-18 18:45:06 +0100

    add small readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChromaAnimation
+
+Simple C# project capable of reading and writing Razer Chroma's `*.chroma` animation files.
--- src/CyberpunkChromaExtractor/Program.cs
+++ src/CyberpunkChromaExtractor/Program.cs
@@ -10,7 +10,7 @@ public static class Program
         //root.json is exported from the game using wolvenKit. It contains all the chroma animations encoded in base64.
         var root = JsonSerializer.Deserialize<Root>(await File.ReadAllTextAsync("root.json"));
 
-        foreach (var set in root!.SetsSerialized)
+        foreach (var set in root.SetsSerialized)
         {
             var setName = set.Name.Value;
             foreach (var animation in set.Animations)
